5f8f4b6b586ac97fd2f1bebe5ee5656bfb122dc4
Arun <arunpanigrahi18@gmail.com>
2023-10-07 03:25:33 +0530

    create signup route and custom error handler

---
+++ api/controller/authController.js
@@ -0,0 +1,15 @@
+import User from "../models/userModel.js";
+import bcryptjs from "bcryptjs";
+
+export const signup=async (req,res,next)=>{
+    const {username,email,password}=req.body;
+    const hashedPassword=bcryptjs.hashSync(password,8);
+    const newUser=new User({username,email,password: hashedPassword});
+    try{
+       await newUser.save();
+       res.status(201).json({message: "user created successfully"});
+    }
+    catch(error){
+        next(error);
+    }
+} 
--- api/index.js
+++ api/index.js
@@ -1,6 +1,7 @@
 import express from "express";
 import mongoose from "mongoose";
 import dotenv from 'dotenv';
+import authRouter from "./routes/authRoute.js";
 dotenv.config();
 
 
@@ -14,6 +15,18 @@ mongoose.connect(process.env.MONGO_URI)
 
 const app=express();
 
+app.use(express.json());
+app.use("/api/auth",authRouter);
+
 app.listen(3000,()=>{
     console.log(`server running on port 3000`);
+})
+
+
+app.use((err,req,res,next)=>{
+    console.log("reaching");
+    const statusCode=err.statusCode || 500;
+    const message=err.message || "Internal server Error";
+    console.log(statusCode,message);
+    res.status(statusCode).json({success: false, statusCode, message});
 })
