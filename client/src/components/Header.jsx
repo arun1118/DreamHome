13fe5cfafb5508648fa4883f0bdf7cc1eab1a167
Arun <arunpanigrahi18@gmail.com>
2023-10-13 23:07:26 +0530

    complete home page

--- client/src/components/Header.jsx
+++ client/src/components/Header.jsx
@@ -30,7 +30,7 @@ const Header = () => {
             <Link to="/">
                 <h1 className='font-bold text-sm sm:text-xl flex flex-wrap'>
                     <span className='text-slate-500'>Dream</span>
-                    <span className='text-slate-700'>House</span>
+                    <span className='text-slate-700'>Home</span>
                 </h1>
             </Link>
             <form onSubmit={handleSubmit} className='bg-slate-100 p-3 rounded-lg flex items-center'>
