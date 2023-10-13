80a2b3e9ba8cb6a11fea3d7449798677a901c244
Arun <arunpanigrahi18@gmail.com>
2023-10-13 13:55:08 +0530

    complete edit-listing functionality

--- client/src/pages/Profile.jsx
+++ client/src/pages/Profile.jsx
@@ -207,7 +207,9 @@ const Profile = () => {
                 </Link>
                 <div className='flex flex-col items-center'>
                   <button onClick={()=> handleListingDelete(listing._id)} className='uppercase text-red-700'>Delete</button>
-                  <button className='uppercase text-green-700'>Edit</button>
+                  <Link to={`/update-listing/${listing._id}`}>
+                    <button className='uppercase text-green-700'>Edit</button>
+                  </Link>
                 </div>
               </div>
             )
